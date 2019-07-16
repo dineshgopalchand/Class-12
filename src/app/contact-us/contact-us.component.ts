99a74967c56799c628266c4ab30fc5b68528ea27
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2019-07-16 09:26:28 +0530

    Template Driven form COmpleted

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -8,9 +8,39 @@ import { NgModel, NgForm } from '@angular/forms';
 })
 export class ContactUsComponent implements OnInit {
 
+  selectedValue = 3;
+  selectedOptObj: any;
+  mailingOption: { id: number, value: string }[] = [
+    {
+      id: 1,
+      value: 'Email'
+    },
+    {
+      id: 2,
+      value: 'SMS'
+    },
+    {
+      id: 3,
+      value: 'Post'
+    },
+    {
+      id: 4,
+      value: 'Call'
+    },
+    {
+      id: 5,
+      value: 'Whatsapp'
+    },
+  ];
   constructor() { }
 
   ngOnInit() {
+    this.selectedOptObj = this.mailingOption.filter(data => {
+      return data.id === this.selectedValue;
+    });
+    // console.log( this.selectedOptObj);
+    this.selectedOptObj = this.selectedOptObj[0];
+    // console.log( this.selectedOptObj);
   }
   inputChange(input: NgModel) {
     console.log(input.errors);
@@ -20,6 +50,6 @@ export class ContactUsComponent implements OnInit {
     // console.log(form.controls);
     console.log(form.value);
     // console.log(form.control);
-    form.control.setErrors({ formError: 'some problem!' });
+    // form.control.setErrors({ formError: 'some problem!' });
   }
 }
