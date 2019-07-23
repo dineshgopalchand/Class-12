fe26c189519940b39b732ce6cbd54c50f83f8574
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2019-07-23 08:42:05 +0530

    Form Array Finished with CRUD operation

--- src/app/to-do-list/to-do-list.component.ts
+++ src/app/to-do-list/to-do-list.component.ts
@@ -17,14 +17,32 @@ export class ToDoListComponent implements OnInit {
   }
   addTopic(topic: HTMLInputElement) {
     // console.log(topic.value);
+    // this.toDoList.push(
+    //   new FormControl(topic.value)
+    // );
     this.toDoList.push(
-      new FormControl(topic.value)
+      new FormGroup({
+        topicname: new FormControl(topic.value),
+        status: new FormControl(false)
+      })
     );
     topic.value = '';
     console.log(this.toDoList);
   }
   get toDoList() {
     return this.toDoForm.get('toDoList') as FormArray;
+  }
+  removeTopic(item: any) {
+    const indexVal = this.toDoList.controls.indexOf(item);
+    console.log(indexVal);
+    // this.toDoList.controls.splice(indexVal, 1);
+    this.toDoList.removeAt(indexVal);
+  }
+  updateTopic(item: any, key: string) {
+    const indexVal = this.toDoList.controls.indexOf(item);
+
+    this.toDoList.controls[indexVal].value[key] = !this.toDoList.controls[indexVal].value[key];
+    console.log(this.toDoList);
   }
 
 }
