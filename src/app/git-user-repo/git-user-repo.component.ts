4ce928fd2ee741273f710f38a9c3a705da49d1f7
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2019-08-11 10:41:33 +0530

    Router Finished

--- src/app/git-user-repo/git-user-repo.component.ts
+++ src/app/git-user-repo/git-user-repo.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { GitFollowerService } from '../common/service/git-follower.service';
+import { combineLatest } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-git-user-repo',
@@ -16,29 +18,84 @@ export class GitUserRepoComponent implements OnInit {
   limit = 10;
   start = 0;
   currentPage = 1;
-  constructor(private route: ActivatedRoute, private gitService: GitFollowerService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private gitService: GitFollowerService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
-    this.route.params
-      .subscribe(param => {
-        console.log(param);
+    // combineLatest(
+    //   this.route.params,
+    //   this.route.queryParamMap
+    // ).subscribe(([param, qParams]) => {
+    //   console.log(param);
+    //   console.log(qParams);
+    //   this.userName = param.username;
+    //   this.userId = param.userid;
+    //   console.log(param.userid);
+    //   console.log(qParams.get('limit'));
+    //   console.log(qParams.get('pagenumber'));
+    //   this.gitService.getRepoList(this.userName)
+    //     .subscribe(repolist => {
+    //       console.log(repolist);
+    //       console.log((repolist as any[]).length);
+    //       this.reposList = repolist;
+    //     });
+    // });
+    combineLatest(
+      this.route.params,
+      this.route.queryParamMap
+    ).pipe(
+      switchMap(([param, qParams]) => {
         this.userName = param.username;
         this.userId = param.userid;
         console.log(param.userid);
-        this.gitService.getRepoList(this.userName)
-          .subscribe(repolist => {
-            console.log(repolist);
-            console.log((repolist as any[]).length);
-            this.reposList = repolist;
-          });
-      });
-    this.route.queryParamMap
-      .subscribe(queryParam => {
-        console.log(queryParam);
-        console.log(queryParam.get('limit'));
-        console.log(queryParam.get('pagenumber'));
-        console.log(queryParam.get('start'));
-      });
+        console.log(qParams.get('limit'));
+        console.log(qParams.get('pagenumber'));
+        return this.gitService.getRepoList(this.userName);
+      })
+    ).subscribe(repolist => {
+      console.log(repolist);
+      console.log((repolist as any[]).length);
+      this.reposList = repolist;
+    });
+
+    // subscribe(([param, qParams]) => {
+    //   console.log(param);
+    //   console.log(qParams);
+    //   this.userName = param.username;
+    //   this.userId = param.userid;
+    //   console.log(param.userid);
+    //   console.log(qParams.get('limit'));
+    //   console.log(qParams.get('pagenumber'));
+    //   this.gitService.getRepoList(this.userName)
+    //     .subscribe(repolist => {
+    //       console.log(repolist);
+    //       console.log((repolist as any[]).length);
+    //       this.reposList = repolist;
+    //     });
+    // });
+    //   this.route.params
+    //     .subscribe(param => {
+    //       console.log(param);
+    //       this.userName = param.username;
+    //       this.userId = param.userid;
+    //       console.log(param.userid);
+    //       this.gitService.getRepoList(this.userName)
+    //         .subscribe(repolist => {
+    //           console.log(repolist);
+    //           console.log((repolist as any[]).length);
+    //           this.reposList = repolist;
+    //         });
+    //     });
+    //   this.route.queryParamMap
+    //     .subscribe(queryParam => {
+    //       console.log(queryParam);
+    //       console.log(queryParam.get('limit'));
+    //       console.log(queryParam.get('pagenumber'));
+    //       // console.log(queryParam.get('start'));
+    //     });
   }
 
   get pageCountArr() {
@@ -46,6 +103,16 @@ export class GitUserRepoComponent implements OnInit {
   }
   get pageCount() {
     return Math.ceil(this.totalNumber / this.limit);
+  }
+
+  homeRedirection() {
+    // this.router.navigateByUrl('/');
+    this.router.navigate(['/'], {
+      queryParams: {
+        source: 'Git-repo',
+        pageno: this.currentPage
+      }
+    });
   }
 
 }
